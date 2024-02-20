3f1ae55881d1959d0c810833675aee5a8b581ecd
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-02-20 22:06:23 +0100

    Update git repo url

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 sudo dnf install -y ansible
-ansible-pull -U git@github.com:0xGuillaume/.bootstrap.git local.yaml
+ansible-pull -U https://github.com/0xGuillaume/.bootstrap.git local.yaml
