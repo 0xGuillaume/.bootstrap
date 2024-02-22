6853c348e5c390fab1c6b7d5a2fe15b568483444
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-02-22 13:19:16 +0100

    Added -K flag

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 sudo dnf install -y ansible
-ansible-pull -U https://github.com/0xGuillaume/.bootstrap.git local.yaml
+ansible-pull -U https://github.com/0xGuillaume/.bootstrap.git local.yaml -K
